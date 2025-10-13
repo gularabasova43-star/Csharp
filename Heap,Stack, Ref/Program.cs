1262494d6d40789ec18209a3bebe04dc885d0329
Gülarə <gularabasova43-star>
2025-10-14 01:51:01 +0400

    praktiki

---
+++ Heap,Stack, Ref/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace Heap_Stack__Ref
+{
+    internal class Program
+    {
+        static void Main()
+        {
+        //    int a = 10;
+        //    Console.WriteLine(a);
+
+        //    EditNumber(ref a);
+        //    Console.WriteLine(a);
+
+        //}
+        //static void EditNumber(ref int number)
+        //{
+        //    number = 50;
+        //    // number+=50;
+        //    //Console.WriteLine(number);
+
+        }
+    }
+}
