429269c84c80dbfc386feb07621fd6a7668c224d
Gülarə <gularabasova43-star>
2025-10-16 01:50:57 +0400

    array tasklar

---
+++ Array, OOP, Class/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Array__OOP__Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numbers= {1,2,3,4,5};
+            //int[] numbers={47,5,63,24,14};
+            //int[] numbers2 = { 1, 2 };
+            //Console.WriteLine(numbers[1]);
+            // int[] numbers = new int[10];
+            // numbers[0] = 10;
+            // numbers[1] = 20;
+            // numbers[2] = 30;
+            // numbers[3] = 88;
+            //// numbers[20] = 44;//20 indeks yoxdur deye sehdir bu yazilis
+            // Array.Resize(ref numbers,20);
+            //Array.Sort(numbers);
+            //Array.Reverse(numbers);
+            //Array.Copy(numbers,numbers2, 2);
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+
+            //Console.WriteLine(numbers[i]);
+            //}
+
+            //int[,] numbers =
+            //{
+            //    {1,2,3 },
+            //    {3,4,2 },
+            //    {5,6,3 }
+
+            //};
+            //Console.WriteLine(numbers[2,1]);
+
+
+            //var obj = new
+            //{
+            //    name = "Gulare",
+            //    surname = "Abasova",
+            //    age = 19,
+            //    adress = "Yasamal"
+            //};
+
+            //var obj1 = new
+            //{
+            //    name = "Nigar",
+            //    surname = "Abasova",
+            //    age = 21,
+            //    adress = "Yasamal"
+            //};
+
+            //var obj2 = new
+            //{
+            //    name = "Jungkook",
+            //    surname = "Jeon",
+            //    age = 28,
+            //    adress = "Koreya"
+            //};
+            //Console.WriteLine(obj.name);
+            //Console.WriteLine(obj1.name);
+            //Console.WriteLine(obj2.name);
+
+            //Console.WriteLine($"name:{obj.name},surname:{obj.surname},age:{obj.age},adress:{obj.adress}");
+            //Console.WriteLine($"name:{obj1.name},surname:{obj1.surname},age:{obj1.age},adress:{obj1.adress}");
+            //Console.WriteLine($"name:{obj2.name},surname:{obj.surname},age:{obj2.age},adress:{obj2.adress}");
+
+
+
+
+            //Student student = new();
+            //student.name = "Gulare";
+            //student.surname = "Abasova";
+            //student.age = 19;
+            //student.adress = "Yasamal";
+
+            //Console.WriteLine(student.name + " " + student.surname);
+        }
+    }
+    //class Student
+    //{
+        //public string name;
+        //public string surname;
+        //public int age;
+        //public string adress;
+
+
+    //}
+
+}
+
